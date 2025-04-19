da6f73430dd074074be3acf9f4cd8a46db17d8b6
dRamee <maksim.yolckin@yandex.com>
2025-04-19 19:17:07 +0700

    2 tasks done

--- src/Task1.hs
+++ src/Task1.hs
@@ -3,49 +3,95 @@
 
 module Task1 where
 
--- | Returns infinite list of natural numbers (excluding zero)
---
--- First 10 natural numbers:
---
--- >>> take 10 nats
--- [1,2,3,4,5,6,7,8,9,10]
---
-nats :: [Integer]
-nats = error "TODO: define nats (Task1)"
+-- Explicit import of Prelude to hide functions
+-- that are not supposed to be used in this assignment
+import Prelude hiding (foldl, foldr)
+-- Import unfoldr for generating lists
+import Data.List (unfoldr)
 
--- | Returns infinite list of fibonacci numbers (starting with zero)
---
--- First 10 fibonacci numbers:
---
--- >>> take 10 fibs
--- [0,1,1,2,3,5,8,13,21,34]
---
-fibs :: [Integer]
-fibs = error "TODO: define fibs (Task1)"
+-- * Type definitions
 
--- | Returns infinite list of prime numbers
---
--- First 10 prime numbers:
---
--- >>> take 10 primes
--- [2,3,5,7,11,13,17,19,23,29]
---
-primes :: [Integer]
-primes = error "TODO: define primes (Task1)"
+-- | Binary tree
+data Tree a = Leaf | Branch a (Tree a) (Tree a)
+  deriving Show
 
--- | One step of Sieve of Eratosthenes
--- (to be used with 'unfoldr')
---
--- Returns next prime number from given list
--- and strikes out all multiples of this prime
--- from the rest of the list
+-- | Forest (i.e. list of 'Tree's)
+type Forest a = [Tree a]
+
+-- | Tree traversal order
+data Order = PreOrder | InOrder | PostOrder
+  deriving Show
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing  = []
+maybeToList (Just x) = [x]
+
+intercalate :: [a] -> [[a]] -> [a]
+intercalate _   []     = []
+intercalate sep (x:xs) = x ++ concatMap (sep ++) xs
+
+-- * Function definitions
+
+-- | Returns values of given 'Tree' in specified 'Order' with optional leaf value
 --
 -- Usage example:
 --
--- >>> sieve [2..20]
--- Just (2,[3,5,7,9,11,13,15,17,19])
--- >>> sieve [3,5..20]
--- Just (3,[5,7,11,13,17,19])
+-- >>> torder PreOrder  (Just '.') (Branch 'A' Leaf (Branch 'B' Leaf Leaf))
+-- "A.B.."
+-- >>> torder InOrder   (Just '.') (Branch 'A' Leaf (Branch 'B' Leaf Leaf))
+-- ".A.B."
+-- >>> torder PostOrder (Just '.') (Branch 'A' Leaf (Branch 'B' Leaf Leaf))
+-- ".BA"
 --
-sieve :: [Integer] -> Maybe (Integer, [Integer])
-sieve = error "TODO: define sieve (Task1)"
+torder :: Order    -- ^ Order of resulting traversal
+       -> Maybe a  -- ^ Optional leaf value
+       -> Tree a   -- ^ Tree to traverse
+       -> [a]      -- ^ List of values in specified order
+
+torder _     m Leaf                = maybeToList m
+torder PreOrder m (Branch a left right) = 
+    a : (torder PreOrder m left ++ torder PreOrder m right)
+torder InOrder  m (Branch a left right) = 
+    torder InOrder m left ++ [a] ++ torder InOrder m right
+torder PostOrder m (Branch a left right) = 
+    torder PostOrder m left ++ torder PostOrder m right ++ [a]
+
+-- | Returns values of given 'Forest' separated by optional separator
+-- where each 'Tree' is traversed in specified 'Order' with optional leaf value
+--
+-- Usage example:
+--
+-- >>> forder PreOrder  (Just '|') (Just '.') [Leaf, Branch 'C' Leaf Leaf, Branch 'A' Leaf (Branch 'B' Leaf Leaf)]
+-- ".|C..|A.B.."
+-- >>> forder InOrder   (Just '|') (Just '.') [Leaf, Branch 'C' Leaf Leaf, Branch 'A' Leaf (Branch 'B' Leaf Leaf)]
+-- ".|.C.|.A.B."
+-- >>> forder PostOrder (Just '|') (Just '.') [Leaf, Branch 'C' Leaf Leaf, Branch 'A' Leaf (Branch 'B' Leaf Leaf)]
+-- ".|..C|...BA"
+--
+forder :: Order     -- ^ Order of tree traversal
+       -> Maybe a   -- ^ Optional separator between resulting tree orders
+       -> Maybe a   -- ^ Optional leaf value
+       -> Forest a  -- ^ List of trees to traverse
+       -> [a]       -- ^ List of values in specified tree order
+forder order msep mleaf forest = intercalate (maybeToList msep) (map (torder order mleaf) forest)
+
+-- * Task 1: Infinite sequences using unfoldr
+
+-- | Infinite list of natural numbers (excluding zero)
+-- First 10 numbers: take 10 nats ==> [1,2,3,4,5,6,7,8,9,10]
+nats :: [Integer]
+nats = unfoldr (\x -> Just (x, x + 1)) 1
+
+-- | Infinite Fibonacci sequence starting with 0
+-- First 10 numbers: take 10 fibs ==> [0,1,1,2,3,5,8,13,21,34]
+fibs :: [Integer]
+fibs = unfoldr (\(a, b) -> Just (a, (b, a + b))) (0, 1)
+
+-- | Infinite list of prime numbers using Sieve of Eratosthenes
+-- First 10 numbers: take 10 primes ==> [2,3,5,7,11,13,17,19,23,29]
+primes :: [Integer]
+primes = unfoldr sieve [2..]
+  where
+    sieve :: [Integer] -> Maybe (Integer, [Integer])
+    sieve (p:xs) = Just (p, filter (\x -> x `mod` p /= 0) xs)
+    sieve []     = Nothing  -- never happens for infinite list
--- src/Task2.hs
+++ src/Task2.hs
@@ -3,84 +3,132 @@
 
 module Task2 where
 
+-- Explicit import of Prelude to hide functions
+-- that are not supposed to be used in this assignment
+import Prelude hiding (compare, foldl, foldr, Ordering(..))
+
+import Task1 (Tree(..))
+import Data.Foldable (Foldable(..), toList)
+
+-- * Type definitions
+
+-- | Ordering enumeration
+-- Overrides Prelude's Ordering
+data Ordering = LT | EQ | GT
+  deriving (Show, Eq)
+
+-- | Binary comparison function indicating whether first argument is less, equal or
+-- greater than the second one (returning 'LT', 'EQ' or 'GT' respectively)
+type Cmp a = a -> a -> Ordering
+
+-- * Function definitions
+
+-- | Binary comparison function induced from `Ord` constraint
+compare :: Ord a => Cmp a
+compare x y
+  | x < y     = LT
+  | x > y     = GT
+  | otherwise = EQ
+
+-- | Conversion of list to binary search tree
+listToBST :: Cmp a -> [a] -> Tree a
+listToBST _ []     = Leaf
+listToBST cmp (x:xs) = tinsert cmp x (listToBST cmp xs)
+
+-- | Conversion from binary search tree to list
+bstToList :: Tree a -> [a]
+bstToList Leaf = []
+bstToList (Branch a left right) = bstToList left ++ [a] ++ bstToList right
+
+-- | Tests whether given tree is a valid binary search tree
+isSorted :: Cmp a -> [a] -> Bool
+isSorted _ [] = True
+isSorted _ [_] = True
+isSorted cmp (x:y:xs) = (cmp x y == LT) && isSorted cmp (y:xs)
+
+isBST :: Cmp a -> Tree a -> Bool
+isBST cmp tree = isSorted cmp (bstToList tree)
+
+-- | Searches given binary search tree for given value
+-- Returns found value wrapped in 'Just' or 'Nothing'
+tlookup :: Cmp a -> a -> Tree a -> Maybe a
+tlookup _ _ Leaf = Nothing
+tlookup cmp x (Branch a left right) = case cmp x a of
+    EQ -> Just a
+    LT -> tlookup cmp x left
+    GT -> tlookup cmp x right
+
+-- | Inserts given value into binary search tree
+-- preserving its BST properties
+tinsert :: Cmp a -> a -> Tree a -> Tree a
+tinsert _ x Leaf = Branch x Leaf Leaf
+tinsert cmp x (Branch a left right) = case cmp x a of
+    LT -> Branch a (tinsert cmp x left) right
+    GT -> Branch a left (tinsert cmp x right)
+    EQ -> Branch x left right
+
+-- | Deletes given value from binary search tree
+-- preserving its BST properties
+tdelete :: Cmp a -> a -> Tree a -> Tree a
+tdelete _ _ Leaf = Leaf
+tdelete cmp x (Branch y left right) = case cmp x y of
+    LT -> Branch y (tdelete cmp x left) right
+    GT -> Branch y left (tdelete cmp x right)
+    EQ -> case (left, right) of
+        (Leaf, _) -> right
+        (_, Leaf) -> left
+        _         -> let m = findMin right
+                     in Branch m left (tdelete cmp m right)
+  where
+    findMin :: Tree a -> a
+    findMin (Branch z Leaf _) = z
+    findMin (Branch _ left' _) = findMin left'
+    findMin Leaf = error "findMin: пустое дерево"
+
+-- * Task 2: Infinite streams
+
 -- | Infinite stream of elements
 data Stream a = Stream a (Stream a)
 
+-- | Functor instance for Stream
+instance Functor Stream where
+  fmap f (Stream x xs) = Stream (f x) (fmap f xs)
+
+-- | Foldable instance for Stream (enables toList)
 instance Foldable Stream where
-  foldMap = error "TODO: define foldMap"
+  foldr f z (Stream x xs) = f x (foldr f z xs)
 
--- | Converts given list into stream
---
--- If the list is finite then it is continued
--- with given value repeated infinitely
---
--- Usage example:
---
--- >>> fromList 0 [1,2,3]
--- [1,2,3,0,0,0,0,0,0,0]
--- >>> fromList undefined [1..]
--- [1,2,3,4,5,6,7,8,9,10]
---
+-- | Show instance: display first 10 elements
+instance Show a => Show (Stream a) where
+  show s = show (take 10 (toList s))
+
+-- | Convert finite list to Stream, repeating the default after end
 fromList :: a -> [a] -> Stream a
-fromList = error "TODO: define fromList"
+fromList def xs = case xs of
+  (y:ys) -> Stream y (fromList def ys)
+  []     -> let s = Stream def s in s
 
--- | Builds stream from given seed value by applying given step function
---
--- Step function produces a pair of the next element in stream and updated seed value.
---
--- Usage example:
---
--- >>> unfold (\x -> (x, x-1)) 5
--- [5,4,3,2,1,0,-1,-2,-3,-4]
--- >>> unfold (\x -> (abs x, x-1)) 5
--- [5,4,3,2,1,0,1,2,3,4]
---
+-- | Build Stream by unfolding seed
 unfold :: (b -> (a, b)) -> b -> Stream a
-unfold = error "TODO: define unfold"
+unfold f b = let (a, b') = f b
+             in Stream a (unfold f b')
 
--- | Returns infinite stream of natural numbers (excluding zero)
---
--- First 10 natural numbers:
---
--- >>> nats
--- [1,2,3,4,5,6,7,8,9,10]
---
+-- | Filter for Stream
+defilter :: (a -> Bool) -> Stream a -> Stream a
+defilter p (Stream x xs)
+  | p x       = Stream x (defilter p xs)
+  | otherwise = defilter p xs
+
+-- | Infinite natural numbers (starting from 1)
 nats :: Stream Integer
-nats = error "TODO: define nats (Task2)"
+nats = unfold (\x -> (x, x + 1)) 1
 
--- | Returns infinite stream of fibonacci numbers (starting with zero)
---
--- First 10 fibonacci numbers:
---
--- >>> fibs
--- [0,1,1,2,3,5,8,13,21,34]
---
+-- | Infinite Fibonacci sequence (0,1,1,2,...)
 fibs :: Stream Integer
-fibs = error "TODO: define fibs (Task2)"
+fibs = unfold (\(a, b) -> (a, (b, a + b))) (0, 1)
 
--- | Returns infinite stream of prime numbers
---
--- First 10 prime numbers:
---
--- >>> primes
--- [2,3,5,7,11,13,17,19,23,29]
---
+-- | Infinite primes using Sieve of Eratosthenes
 primes :: Stream Integer
-primes = error "TODO: define primes (Task2)"
-
--- | One step of Sieve of Eratosthenes
--- (to be used with 'unfoldr')
---
--- Returns next prime number from given stream
--- and strikes out all multiples of this prime
--- from the rest of the stream
---
--- Usage example:
---
--- >>> sieve $ fromList 0 [2..]
--- (2,[3,5,7,9,11,13,15,17,19,21])
--- >>> sieve $ snd $ sieve $ fromList 0 [2..]
--- (3,[5,7,11,13,17,19,23,25,29,31])
---
-sieve :: Stream Integer -> (Integer, Stream Integer)
-sieve = error "TODO: define sieve (Task2)"
+primes = sieve (unfold (\x -> (x, x + 1)) 2)
+  where
+    sieve (Stream p ps) = Stream p (sieve (defilter (\x -> x `mod` p /= 0) ps))
